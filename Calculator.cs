8bf4e7eee3afd406802a2c2e7522378ad51be02b
Zelfire <raymond_hong1@yahoo.com>
2015-06-09 20:45:14 -0700

    Fixed stuff after the merge. Git merge is evil.

--- Calculator.cs
+++ Calculator.cs
@@ -21,33 +21,33 @@ namespace MultiDesktop
         private readonly string SIN = "sin";
         private readonly string COS = "cos";
         private readonly string TAN = "tan";
-        private readonly string COSEC = "cosec";
+        private readonly string COTAN = "cot";
         private readonly string SEC = "sec";
-        private readonly string COTAN = "cotan";
+        private readonly string COSEC = "cosec";
         private readonly string LOG = "log";
+        private readonly string LN = "ln";
         private readonly string FACTORIAL = "!";
         private readonly string ASIN = "asin";
         private readonly string ACOS = "acos";
         private readonly string ATAN = "atan";
-        private readonly string ACOSEC = "acosec";
+        private readonly string ACOTAN = "acot";
         private readonly string ASEC = "asec";
-        private readonly string ACOTAN = "acotan";
+        private readonly string ACOSEC = "acosec";
         private readonly string SINH = "sinh";
         private readonly string COSH = "cosh";
         private readonly string TANH = "tanh";
-        private readonly string LN = "ln";
-        private readonly string NEGATIVE = "-"; //This is a hyphen
+        private readonly string COTANH = "cotanh";
+        private readonly string SECH = "sech";
+        private readonly string COSECH = "cosech";
         private readonly string SQRT = "√";
         private readonly string EQUAL = "=";
+        private readonly string NEGATIVE = "-"; //This is a hyphen
+
         private readonly string precedence2 = "+−";
         private readonly string precedence3 = "*/%";
-        private readonly string COSECH = "cosech";
-        private readonly string SECH = "sech";
-        private readonly string COTANH = "cotanh";
-        private readonly string NEGATIVE = "-"; //This is a hyphen
-        private readonly string SQRT = "√";
-        private readonly string precedence4 = "^!sincostanloglnasinacosatansinhcoshtanh-√"; //So ugly. Will think of better way later.
-        private readonly string[] tokenList = { "sin", "cos", "tan", "log", "ln", "asin", "acos", "atan", "sinh", "cosh", "tanh" };
+        private readonly string precedence4 = "^!sincostancotseccosecloglnasinacosatanacotasecacosecsinhcoshtanhcotanhsechcosech-√"; //So ugly. Will think of better way later.
+       
+        private readonly string[] tokenList = { "sin", "cos", "tan", "cot", "sec", "cosec", "log", "ln", "asin", "acos", "atan", "acot", "asec", "acosec", "sinh", "cosh", "tanh", "cotanh", "sech", "cosech" };
 
 
         public void clear()
@@ -75,11 +75,9 @@ namespace MultiDesktop
             List<string> postFixed = convert(input);
             var stack = new Stack<string>();
 
-
             for (int i = 0; i < postFixed.Count; i++)
             {
                 double retNum;
-
                 if (Double.TryParse(postFixed[i], out retNum)) //If incoming character is a number
                 {                    
                     stack.Push(postFixed[i]);
@@ -116,7 +114,6 @@ namespace MultiDesktop
          */
         private double operate(string anOperand1, string anOperand2, string anOperator)
         {
-
             double operand1 = Convert.ToDouble(anOperand1);
             double operand2 = Convert.ToDouble(anOperand2);
             double output = 0;
@@ -254,21 +251,6 @@ namespace MultiDesktop
             else if (function.Equals(LOG))
             {
                 return Math.Log10(operand);
-            }
-
-            else if (function.Equals(LN))
-            {
-                return Math.Log(operand, Math.E);
-            }
-
-            else if (function.Equals(NEGATIVE))
-            {
-                return 0.0 - operand;
-            }
-
-            else
-            {
-                return Math.Sqrt(operand);
             }
 
             else if (function.Equals(LN))
